e6669817d17f940c609198c875fda3f397f454a8
shan1iu <mountainliuliu@gmail.com>
2018-04-25 19:40:27 +0100

    project finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-import "./App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
+import NavBar from "./components/navbar/NavBar";
+import Search from "./components/search/Search";
+
+import "./App.css";
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
